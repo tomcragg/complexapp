5738daae9087be209f65690ada7c5dc5f7086881
Tom <tom.cragg1@gmail.com>
2020-05-28 22:44:24 +0200

    Registration form complete.

--- app/Main.js
+++ app/Main.js
@@ -86,6 +86,26 @@ function Main() {
     }
   }, [state.loggedIn])
 
+  // Check if token has expired or not on first render
+  useEffect(() => {
+    if (state.loggedIn) {
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/checkToken", { token: state.user.token }, { cancelToken: ourRequest.token })
+          if (!response.data) {
+            dispatch({ type: "logout" })
+            dispatch({ type: "flashMessage", value: "Your session has expired. Please log in again." })
+          }
+        } catch (e) {
+          console.log("There wasa problem or the request was cancelled.")
+        }
+      }
+      fetchResults()
+      return () => ourRequest.cancel()
+    }
+  }, [])
+
   return (
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
